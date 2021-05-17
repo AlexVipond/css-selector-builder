d2d9ec56028e578d621e209e719f1844be0df54a
AlexVipond <alex.r.vipond@gmail.com>
2021-05-17 12:45:56 -0600

    generate types for package

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import pluginVue from '@vitejs/plugin-vue'
 
 export default {
   plugins: [
-    pluginVue()
+    pluginVue(),
   ],
 }
--- vite.lib.config.ts
+++ vite.lib.config.ts
@@ -2,7 +2,8 @@ export default {
   build: {
     lib: {
       entry: 'src/pipes/index.ts',
-      name: 'CssSelectorPipes',  
+      name: 'CssSelectorPipes',
+      fileName: 'index',
     },
     outDir: 'lib',
   }
